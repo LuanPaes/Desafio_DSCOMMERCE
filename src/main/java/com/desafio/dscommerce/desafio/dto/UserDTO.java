50cac77bfad56a06567ce847583d3ebde665ecb3
Luan M Paes <luanmp25@gmail.com>
2025-11-04 17:28:56 -0300

    Revert "Dtos Criados"
    This reverts commit 967ce7beee808e02963d69c83dec0b8336546bf6.

--- src/main/java/com/desafio/dscommerce/desafio/dto/UserDTO.java
+++
@@ -1,5 +0,0 @@
-package com.desafio.dscommerce.desafio.dto;
-
-public class UserDTO {
-
-}
--- src/main/java/com/desafio/dscommerce/desafio/entities/Payment.java
+++ src/main/java/com/desafio/dscommerce/desafio/entities/Payment.java
@@ -1,16 +1,9 @@
 package com.desafio.dscommerce.desafio.entities;
 
+import jakarta.persistence.*;
+
 import java.time.Instant;
 import java.util.Objects;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_payment")
